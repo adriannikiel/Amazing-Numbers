70152238d24284c168d2c8b89c4aa0c9f8c293d5
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-02-27 00:59:39 +0100

    finished 8th stage of project

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -22,10 +22,16 @@ public class Main {
             int times = Integer.parseInt(inputs[2]);
             String[] propertiesAsString = Arrays.copyOfRange(inputs, 3, inputs.length);
 
-            List<NumberProperty> properties = new ArrayList<>();
+            List<NumberProperty> expectedProperties = new ArrayList<>();
+            List<NumberProperty> excludedProperties = new ArrayList<>();
 
-            for (int i = 0; i < propertiesAsString.length; i++) {
-                properties.add(NumberProperty.valueOf(propertiesAsString[i]));
+            for (String property : propertiesAsString) {
+                if (property.startsWith("-")) {
+                    property = property.substring(1);
+                    excludedProperties.add(NumberProperty.valueOf(property));
+                } else {
+                    expectedProperties.add(NumberProperty.valueOf(property));
+                }
             }
 
             if (number == 0) {
@@ -62,7 +68,7 @@ public class Main {
                 } else if (times < 1) {
                     printSecondWrongParameterWarning();
                 } else {
-                    printSearchNumberProperties(number, times, properties);
+                    printSearchNumberProperties(number, times, expectedProperties, excludedProperties);
                 }
             }
         } while (true);
@@ -135,21 +141,40 @@ public class Main {
 
     private static boolean validateProperties(String[] properties) {
 
+        if (!checkWrongProperty(properties)) {
+            return false;
+        }
+
+        if (!checkMutualExclusivity(properties)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean checkWrongProperty(String[] propertiesAsString) {
         List<String> wrongProperties = new ArrayList<>();
 
-        for (String property : properties) {
+        for (String property : propertiesAsString) {
 
             boolean foundProperty = false;
 
             for (NumberProperty value : NumberProperty.values()) {
-                if (property.toUpperCase().equals(value.name())) {
+
+                if (property.startsWith("-")) {
+                    property = property.substring(1).toUpperCase();
+                } else {
+                    property = property.toUpperCase();
+                }
+
+                if (property.equals(value.name())) {
                     foundProperty = true;
                     break;
                 }
             }
 
             if (!foundProperty) {
-                wrongProperties.add(property.toUpperCase());
+                wrongProperties.add(property);
             }
         }
 
@@ -158,20 +183,53 @@ public class Main {
             return false;
         }
 
-        String propertiesAsString = Arrays.toString(properties).toUpperCase();
+        return true;
+    }
 
-        if (propertiesAsString.contains("EVEN") && propertiesAsString.contains("ODD")) {
-            printMutuallyExclusivePropertyParameterWarning("EVEN", "ODD");
+    private static boolean checkMutualExclusivity(String[] propertiesAsString) {
+        List<String> properties = new ArrayList<>();
+
+        for (String p : propertiesAsString) {
+            properties.add(p.toUpperCase());
+        }
+
+        if (!checkMutualExclusivityProperty(properties, "EVEN", "ODD")) {
             return false;
         }
 
-        if (propertiesAsString.contains("DUCK") && propertiesAsString.contains("SPY")) {
-            printMutuallyExclusivePropertyParameterWarning("DUCK", "SPY");
+        if (!checkMutualExclusivityProperty(properties, "-EVEN", "-ODD")) {
             return false;
         }
 
-        if (propertiesAsString.contains("SUNNY") && propertiesAsString.contains("SQUARE")) {
-            printMutuallyExclusivePropertyParameterWarning("SUNNY", "SQUARE");
+        if (!checkMutualExclusivityProperty(properties, "DUCK", "SPY")) {
+            return false;
+        }
+
+        if (!checkMutualExclusivityProperty(properties, "-DUCK", "-SPY")) {
+            return false;
+        }
+
+        if (!checkMutualExclusivityProperty(properties, "SUNNY", "SQUARE")) {
+            return false;
+        }
+
+        if (!checkMutualExclusivityProperty(properties, "-SUNNY", "-SQUARE")) {
+            return false;
+        }
+
+        for (NumberProperty value : NumberProperty.values()) {
+            if (!checkMutualExclusivityProperty(properties, value.name(), "-" + value.name())) {
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+
+    private static boolean checkMutualExclusivityProperty(List<String> properties, String prop1, String prop2) {
+        if (properties.contains(prop1) && properties.contains(prop2)) {
+            printMutuallyExclusivePropertyParameterWarning(prop1, prop2);
             return false;
         }
 
@@ -252,12 +310,45 @@ public class Main {
             number /= 10;
             int current = (int) (number % 10);
 
-            if ( (previous - 1 != current) && (previous + 1 != current)) {
+            if ((previous - 1 != current) && (previous + 1 != current)) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    private static boolean isHappyNumber(long number) {
+
+        long result = number;
+
+        while (result != 1 && result != 4) {
+            result = countHappyNumber(result);
+        }
+
+        if (result == 1) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static long countHappyNumber(long number) {
+        long rem = 0;
+        long sum = 0;
+
+        while (number > 0) {
+            rem = number % 10;
+            sum = sum + (rem * rem);
+            number = number / 10;
+        }
+        return sum;
+    }
+
+
+    private static boolean isSadNumber(long number) {
+        return !isHappyNumber(number);
     }
 
     private static List<NumberProperty> checkNumbers(long number) {
@@ -274,6 +365,8 @@ public class Main {
         if (isSunnyNumber(number)) properties.add(NumberProperty.SUNNY);
         if (isSquareNumber(number)) properties.add(NumberProperty.SQUARE);
         if (isJumpingNumber(number)) properties.add(NumberProperty.JUMPING);
+        if (isHappyNumber(number)) properties.add(NumberProperty.HAPPY);
+        if (isSadNumber(number)) properties.add(NumberProperty.SAD);
 
         return properties;
     }
@@ -293,6 +386,8 @@ public class Main {
         System.out.println("\tsunny: " + properties.contains(NumberProperty.SUNNY));
         System.out.println("\tsquare: " + properties.contains(NumberProperty.SQUARE));
         System.out.println("\tjumping: " + properties.contains(NumberProperty.JUMPING));
+        System.out.println("\thappy: " + properties.contains(NumberProperty.HAPPY));
+        System.out.println("\tsad: " + properties.contains(NumberProperty.SAD));
     }
 
     private static void printMultipleNumberProperties(long number, int times) {
@@ -311,6 +406,8 @@ public class Main {
             System.out.print(properties.contains(NumberProperty.SUNNY) ? " sunny," : "");
             System.out.print(properties.contains(NumberProperty.SQUARE) ? " square," : "");
             System.out.print(properties.contains(NumberProperty.JUMPING) ? " jumping," : "");
+            System.out.print(properties.contains(NumberProperty.HAPPY) ? " happy," : "");
+            System.out.print(properties.contains(NumberProperty.SAD) ? " sad," : "");
             System.out.print(properties.contains(NumberProperty.EVEN) ? " even" : "");
             System.out.print(properties.contains(NumberProperty.ODD) ? " odd" : "");
 
@@ -318,26 +415,28 @@ public class Main {
         }
     }
 
-    private static void printSearchNumberProperties(long number, int times, List<NumberProperty> properties) {
+    private static void printSearchNumberProperties(long number, int times, List<NumberProperty> expectedProperties, List<NumberProperty> excludedProperties) {
 
         for (int i = 0; i < times; i++) {
-            number = findNextNumberWithProperties(properties, number);
+            number = findNextNumberWithProperties(expectedProperties, excludedProperties, number);
             printMultipleNumberProperties(number, 1);
             number++;
         }
 
     }
 
-    private static long findNextNumberWithProperties(List<NumberProperty> expectedProperties, long number) {
+    private static long findNextNumberWithProperties(List<NumberProperty> expectedProperties, List<NumberProperty> excludedProperties, long number) {
 
         while (true) {
             List<NumberProperty> properties = checkNumbers(number);
 
-            if (properties.containsAll(expectedProperties)) {
+            if (properties.containsAll(expectedProperties) &&
+                    (excludedProperties.size() == 0 || !properties.containsAll(excludedProperties))) {
                 break;
             } else {
                 number++;
             }
+
         }
 
         return number;
@@ -345,12 +444,12 @@ public class Main {
 
     private static void printInstructions() {
         System.out.println("Supported requests:\n" +
-                "- enter a natural number to know its properties; \n" +
+                "- enter a natural number to know its properties;\n" +
                 "- enter two natural numbers to obtain the properties of the list:\n" +
                 "  * the first parameter represents a starting number;\n" +
                 "  * the second parameter shows how many consecutive numbers are to be printed;\n" +
-                "- two natural numbers and a property to search for;\n" +
-                "- two natural numbers and two properties to search for;\n" +
+                "- two natural numbers and properties to search for;\n" +
+                "- a property preceded by minus must not be present in numbers;\n" +
                 "- separate the parameters with one space;\n" +
                 "- enter 0 to exit.\n");
     }
@@ -370,7 +469,7 @@ public class Main {
             System.out.println("The properties " + properties + " are wrong.");
         }
 
-        System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY, JUMPING]");
+        System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY, JUMPING, HAPPY, SAD]");
     }
 
     private static void printMutuallyExclusivePropertyParameterWarning(String property1, String property2) {
